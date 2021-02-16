c16d703ffaafa8dce53526416ea205a25f1bceee
ren <rrzkk@live.com>
2021-02-16 16:30:51 +1100

    material UI

--- contants/contant.js
+++ contants/contant.js
@@ -1,2 +1,16 @@
 export const INTROTITLE='Super Hero'
 export const INTROTEXT='Please set up your superhero account with us. If you already got an account you can log in derictly, if not, please be patient to stay here for seconds. Superhero is always patient, right?'
+export const USERTYPE = [
+    {
+      value: "personal",
+      label: "personal",
+    },
+    {
+      value: "educational",
+      label: "educational",
+    },
+    {
+      value: "commercial",
+      label: "commercial",
+    },
+  ];
--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,31 @@
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import OutlinedInput from "@material-ui/core/Input";
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+
 import React from "react";
 import Link from "next/link";
 import Pageright from "./components/pageright";
+import * as constant from "../contants/contant";
+
+
 
 export default function Signup() {
+  const [values, setValues] = React.useState({
+    amount: "",
+    showPassword: false,
+  });
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="container">
       <div className="pageleft">
@@ -14,16 +37,52 @@ export default function Signup() {
               <a> log in</a>
             </Link>
           </p>
-          <form >
-            <input type="text"></input>
-            <input type="text"></input>
-            <select>
-              <option>Grapefruit</option>
-              <option>Lime</option>
-              <option>Coconut</option>
-              <option>Mango</option>
-            </select>
-            <input type="text"></input>
+          <form>
+            <TextField
+              style={{ marginBottom: "20px" }}
+              label="Your name"
+              variant="outlined"
+            />
+            <TextField
+              style={{ marginBottom: "20px" }}
+              label="Email address"
+              variant="outlined"
+            />
+            <TextField
+              style={{ marginBottom: "20px" }}
+              select
+              label="I would describe my user type as"
+              variant="outlined"
+            >
+              {constant.USERTYPE.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <FormControl
+              variant="outlined"
+            >
+              <InputLabel htmlFor="outlined-adornment-password">
+                Password
+              </InputLabel>
+              <OutlinedInput
+                style={{ marginBottom: "20px" }}
+                label="Password"
+                type={values.showPassword ? "text" : "password"}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
           </form>
           <p>
             By clicking the "Next" button, you agree to creating a free account,
